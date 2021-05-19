cfe59abeb3015ddd4d7aad6ed6f2bcdb536dc0a7
BelongaRobert <68036456+BelongaRobert@users.noreply.github.com>
2021-05-19 10:40:54 -0400

    RB: Am trying to get project structure set up

--- calculatorjava/src/com/company/Arrayex.java
+++ calculatorjava/src/com/company/Arrayex.java
@@ -10,8 +10,7 @@ import java.util.List;
 public class Arrayex {
 
 	public static void main(String[] args) {
-		int nums[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-//		System.out.println(Arrays.toString(nums));
+		//		System.out.println(Arrays.toString(nums));
 		matrix();
 		sandwiches();
 		favoriteClass();
